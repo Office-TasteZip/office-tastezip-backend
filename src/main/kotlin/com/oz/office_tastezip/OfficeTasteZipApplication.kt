0328ad0ea099b2b623af23ebcc59c15ba05378b9
이원근 <leewg97@naver.com>
2025-05-16 15:02:46 +0900

    initial commit

---
+++ src/main/kotlin/com/oz/office_tastezip/OfficeTasteZipApplication.kt
@@ -0,0 +1,11 @@
+package com.oz.office_tastezip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OfficeTasteZipApplication
+
+fun main(args: Array<String>) {
+    runApplication<OfficeTasteZipApplication>(*args)
+}
---
+++ src/test/kotlin/com/oz/office_tastezip/OfficeTastezipApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oz.office_tastezip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OfficeTastezipApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
