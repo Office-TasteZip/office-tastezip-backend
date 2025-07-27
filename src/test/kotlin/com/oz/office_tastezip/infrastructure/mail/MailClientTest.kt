a28d358f96bbc26c874015320a018f2e6cffef5f
이원근 <leewg97@naver.com>
2025-07-27 23:36:30 +0900

    test #4: MailClientTest 작성

---
+++ src/test/kotlin/com/oz/office_tastezip/infrastructure/mail/MailClientTest.kt
@@ -0,0 +1,35 @@
+package com.oz.office_tastezip.infrastructure.mail
+
+import com.oz.office_tastezip.global.util.MailProperties
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldNotBe
+import io.mockk.*
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+
+class MailClientTest : StringSpec({
+
+    val mailSender = mockk<JavaMailSender>(relaxUnitFun = true)
+    val mailProps = MailProperties(username = "test@example.com")
+    val mailClient = MailClient(mailSender, mailProps)
+
+    "메일 전송" {
+        val to = "recipient@example.com"
+        val subject = "Test Subject"
+        val content = "Test Content"
+
+        val slot = slot<SimpleMailMessage>()
+        every { mailSender.send(capture(slot)) } just runs
+
+        mailClient.sendSimpleMail(to, subject, content)
+
+        verify(exactly = 1) { mailSender.send(any<SimpleMailMessage>()) }
+
+        slot.captured.apply {
+            from shouldNotBe null
+            to shouldNotBe null
+            this.subject shouldNotBe null
+            text shouldNotBe null
+        }
+    }
+})
